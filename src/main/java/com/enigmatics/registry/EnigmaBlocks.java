3dd3005082d32c59552fcae7588a1ba32954c455
TimeConqueror <timeconqueror999@gmail.com>
2020-07-12 04:41:07 +0300

    Added custom creative group

--- src/main/java/com/enigmatics/registry/EnigmaBlocks.java
+++ src/main/java/com/enigmatics/registry/EnigmaBlocks.java
@@ -1,9 +1,9 @@
 package com.enigmatics.registry;
 
+import com.enigmatics.EnigmaGroup;
 import com.enigmatics.Enigmatics;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import ru.timeconqueror.timecore.registry.TimeAutoRegistrable;
 import ru.timeconqueror.timecore.registry.deferred.DeferredBlockRegister;
@@ -14,6 +14,6 @@ public class EnigmaBlocks {
 
     public static final RegistryObject<Block> METEOR_BLOCK = REGISTER.regBlock("meteor_block", () -> new Block(Block.Properties.create(Material.ROCK)))
             .genDefaultStateAndModel()
-            .regDefaultBlockItem(ItemGroup.MISC)
+            .regDefaultBlockItem(EnigmaGroup.INSTANCE)
             .endTyped();
 }
